467ba55345c9495d6880f9f0c6c46a10bc459b10
Luke S <hyperconix@pm.me>
2023-04-07 21:28:49 +0100

    refactor: add clarification to print about spaces

--- src/constituency_check.py
+++ src/constituency_check.py
@@ -11,7 +11,7 @@ def main():
     if len(sys.argv) < 2:
         print('Postcode not provided. Please provide a postcode\n%s' % USAGE)
     elif re.match(POSTCODE_REGEX, sys.argv[1]) == None:
-        print("You did not provide a valid UK Postcode. Example: FK20JA\n%s" % USAGE)
+        print("You did not provide a valid UK Postcode. Example: FK20JA (do not include a space)\n%s" % USAGE)
     else:
         scrape_seat_details(sys.argv[1])
 
