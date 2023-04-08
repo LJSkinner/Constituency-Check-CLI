f1e26700024ae6a57bc5cf67ee91269c998ceb5c
Luke S <hyperconix@pm.me>
2023-04-08 09:11:36 +0100

    feat: add function to get bs4 soup using URL

--- src/constituency_check.py
+++ src/constituency_check.py
@@ -1,5 +1,5 @@
-import bs4 
-import requests
+import bs4 as bs
+import urllib.request as request
 import sys
 import re
 
@@ -7,16 +7,25 @@ USAGE = 'Usage: python3 constiuency_check.py <postcode>'
 
 POSTCODE_REGEX = '^[A-Z]{1,2}[0-9][A-Z0-9]?[0-9][A-Z]{2}$'
 
+URL = 'https://www.electoralcalculus.co.uk/fcgi-bin/seatdetails.py?postcode='
+
 def main():
     if len(sys.argv) < 2:
         print('Postcode not provided. Please provide a postcode\n%s' % USAGE)
     elif re.match(POSTCODE_REGEX, sys.argv[1]) == None:
         print("You did not provide a valid UK Postcode. Example: FK20JA (do not include a space)\n%s" % USAGE)
     else:
+        soup = get_soup(sys.argv[1])
+        
         process_seat_details(sys.argv[1])
 
 def process_seat_details(postcode: str):
     pass
+
+def get_soup(postcode: str) -> bs.BeautifulSoup:
+    source = request.urlopen(URL + postcode).read()
+    
+    return bs.BeautifulSoup(source, "lxml")
         
 if __name__ == '__main__':
     main()  
